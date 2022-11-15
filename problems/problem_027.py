74885af9487a4f8e462d3618644885da6760ea57
Esther Kim <kim.esther117@gmail.com>
2022-11-14 16:00:52 -0800

    Solved Problem 27

--- problems/problem_027.py
+++ problems/problem_027.py
@@ -5,4 +5,10 @@
 #
 
 def max_in_list(values):
-    pass
+    if len(values) == 0:
+        return None
+    max = values[0]
+    for value in values:
+        if value > max:
+            max = value
+    return max
