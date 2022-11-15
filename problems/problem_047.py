6685636eb33bcda03c802ae4ca078d1e19204525
Esther Kim <kim.esther117@gmail.com>
2022-11-15 15:56:43 -0800

    Solved Problem 47

--- problems/problem_047.py
+++ problems/problem_047.py
@@ -12,5 +12,26 @@
 # The string object has some methods that you may want to use,
 # like ".isalpha", ".isdigit", ".isupper", and ".islower"
 
+spec_char = "~!@#$%^&*_-+=`|\(){}[]:;'<>,.?/"
+
+
 def check_password(password):
-    pass
+    check_lower = False
+    check_upper = False
+    check_digit = False
+    check_spec = False
+    check_length = False
+
+    for pw in password:
+        if pw.islower():
+            check_lower = True
+        if pw.isupper():
+            check_upper = True
+        if pw.isdigit():
+            check_digit = True
+        for char in spec_char:
+            if char == pw:
+                check_spec = True
+        if len(password) in range(6, 12):
+            check_length = True
+    return check_lower and check_upper and check_digit and check_spec and check_length
