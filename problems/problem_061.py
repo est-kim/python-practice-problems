a0138bba3e5e71ecfaa358ddc1fa78628d21ca05
Esther Kim <kim.esther117@gmail.com>
2022-11-16 11:56:36 -0800

    Solved Problem 61

--- problems/problem_061.py
+++ problems/problem_061.py
@@ -14,3 +14,9 @@
 #     * input:   [1, 3, 3, 20, 3, 2, 2]
 #       returns: [1, 3, 20, 2]
 
+def remove_duplicates(values):
+    new_list = []
+    for value in values:
+        if value not in new_list:
+            new_list.append(value)
+    return new_list
