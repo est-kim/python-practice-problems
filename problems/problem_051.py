25aebb357cbba72c19afa549c9078ffa586ae5a4
Esther Kim <kim.esther117@gmail.com>
2022-11-15 17:41:00 -0800

    Solved Problem 51

--- problems/problem_051.py
+++ problems/problem_051.py
@@ -7,3 +7,15 @@
 #
 # Don't for get to import math!
 
+import math
+
+
+def safe_divide(numer, denom):
+    if denom == 0:
+        return math.inf
+    else:
+        return numer / denom
+
+
+
+#resource: https://www.includehelp.com/python/math-inf-constant-with-example.aspx
