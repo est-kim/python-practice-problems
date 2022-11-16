3c1e0879a8c1526649f08762d0ea289af298a129
Esther Kim <kim.esther117@gmail.com>
2022-11-16 12:58:02 -0800

    Solved Problem 63

--- problems/problem_063.py
+++ problems/problem_063.py
@@ -21,3 +21,20 @@
 # You may want to look at the built-in Python functions
 # "ord" and "chr" for this problem
 
+def shift_letters(word):
+    new_str = ""
+    new_list = []
+    for letter in word:
+        new_list.append(ord(letter))
+    for num in new_list:
+        new_str += chr(num + 1)
+        for r in (("{", "a"), ("[", "A")):
+            new_str = new_str.replace(*r)
+    return new_str
+
+#resources:
+#https://stackoverflow.com/questions/16060899/alphabet-range-in-python (importing list of alphabets)
+#https://stackoverflow.com/questions/50826831/python-append-to-an-empty-string
+#https://www.geeksforgeeks.org/ord-function-python/
+#https://www.geeksforgeeks.org/chr-in-python/
+#https://stackoverflow.com/questions/6116978/how-to-replace-multiple-substrings-of-a-string
